5e872c82727a1d1eea6403a6454fdc79bdfb26e5
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-01-29 07:57:57 +0100

    throw in case of wrong configuration

--- index.js
+++ index.js
@@ -4,39 +4,33 @@
 const fs = require('fs');
 const path = require('path');
 
-
-const fatal = require('./lib/log-fatal-error');
 const filterObj = require('./lib/filter-object');
 const type = require('./lib/get-type');
 
 
-function validateSettings(settings){
-
-  if (!settings.collection){
-    throw new TypeError('The "collection" setting must be specified');
-  }
-
-  if (!settings.layoutName){
-    throw new TypeError('The "layoutName" setting must be specified');
-  }
-
-  if (!settings.paginationTemplatePath){
-    throw new TypeError('The "paginationTemplatePath" setting must be specified');
-  }
-
-  if (type(settings.elementsPerPage) != 'number'){
-    throw new TypeError('The "elementsPerPage" setting must be specified as a number');
-  }
-
-}
 
 exports = module.exports = function pager(options){
 
-  try{
-    validateSettings(options);
-  } catch(err){
-    return void fatal(err.message);
+
   }
+
+
+  if (!options.collection){
+    throw new Error('The "collection" setting must be specified')
+  }
+
+  if (!options.layoutName){
+    throw new Error('The "layoutName" setting must be specified');
+  }
+
+  if (!options.paginationTemplatePath){
+    throw new Error('The "paginationTemplatePath" setting must be specified');
+  }
+
+  if (type(options.elementsPerPage) != 'number'){
+    throw new Error('The "elementsPerPage" setting must be specified as a number');
+  }
+
 
   return function(files, metalsmith, done){
 
@@ -44,19 +38,9 @@ exports = module.exports = function pager(options){
     const elementsPerPage = options.elementsPerPage || 5;
     const pageLabel = options.pageLabel || ':PAGE';
 
-    const paginationTemplatePath = path.join(metalsmith._source, options.paginationTemplatePath);
 
-    try{
-      // check the pagination template exists,
-      // and the user has the rights to read its content
-      fs.accessSync(paginationTemplatePath, fs.R_OK);
-    }
-    catch(err){
-      return void fatal(err.message);
-    }
+    const template = fs.readFileSync(path.join(metalsmith._source, options.paginationTemplatePath));
 
-
-    const template = fs.readFileSync(paginationTemplatePath);
 
     const groupedPosts = filterObj(files, function(all, k){
       return Array.isArray(all[k].collection) && all[k].collection.indexOf(options.collection) >= 0;
